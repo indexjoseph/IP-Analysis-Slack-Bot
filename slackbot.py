b56ce7a50696ad084fac1dddbf19d3c770ce1cd8
indexjoseph <joladeji03@gmail.com>
2022-08-05 12:46:34 -0400

    Adding event handler

--- slackbot.py
+++ slackbot.py
@@ -1,13 +1,24 @@
-import slack, os
+from re import A
+import slack, os 
 from dotenv import load_dotenv
 from pathlib import Path
+from slackeventsapi import SlackEventAdapter
+from flask import Flask
+
 
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
-print("Hello! This is a slack bot that has the abilities to receieve and send messages!")
-print("\n" + "Please Enter a Message you would like to the bot to send")
-chatText = str(input())
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-for i in range(3):
-    client.chat_postMessage(channel='#response-zone', text=chatText)
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/events", app)
+
+# print("Hello! This is a slack bot that has the abilities to receieve and send messages!")
+# print("\n" + "Please Enter a Message you would like to the bot to send")
+
+
+# client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+# chatText = str(input())
+# client.chat_postMessage(channel='#response-zone', text=chatText)
+
+if __name__ == "__main__":
+    app.run(debug=True)
